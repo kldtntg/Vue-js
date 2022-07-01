11af7e59e57a1de761c3f8195b5b32a660e77aa9
Khai Lam Nguyen <kldtntg@gmail.com>
2022-07-01 14:59:56 +0700

    Initial commit

---
+++ script2.js
@@ -0,0 +1,14 @@
+document.getElementById("first").addEventListener('click', function(){window.location.href = "index_mac1.html";})
+
+
+
+document.getElementById("second").addEventListener('click', function(){window.location.href = "index_mac1.html";})
+
+document.getElementById("third").addEventListener('click', function(){window.location.href = "index_mac1.html";})
+
+document.getElementById("fourth").addEventListener('click', function(){window.location.href = "index_mac1.html";})
+
+document.getElementById("fifth").addEventListener('click', function(){window.location.href = "index_mac1.html";})
+
+document.getElementById("sixth").addEventListener('click', function(){window.location.href = "index_mac1.html";})
+
