f4a4769ac31a9728e63bc789e33e10ef11b41904
Khai Lam Nguyen <kldtntg@gmail.com>
2022-07-05 10:20:08 +0700

    config vue router

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,9 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
-import HomeView from "../views/HomeView.vue";
+
+import HomeView from "@/views/HomeView.vue";
+import ProductDetailView from "@/views/ProductDetailView.vue";
+import ProductListView from "@/views/ProductListView.vue";
 
 Vue.use(VueRouter);
 
@@ -11,13 +14,14 @@ const routes = [
     component: HomeView,
   },
   {
-    path: "/about",
-    name: "about",
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () =>
-      import(/* webpackChunkName: "about" */ "../views/AboutView.vue"),
+    path: "/products",
+    name: "products",
+    component: ProductListView,
+  },
+  {
+    path: "/products/detail",
+    name: "product-detail",
+    component: ProductDetailView,
   },
 ];
 
---
+++ src/views/ProductListView.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <h1>Product List View</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "ProductListViews",
+};
+</script>
+
+<style></style>
